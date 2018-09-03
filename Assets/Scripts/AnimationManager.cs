c6c6b9d37c409ca468abb95662971431d3ab4984
fatymahmed <e2107613@ceng.metu.edu.tr>
2018-09-03 15:01:55 +0300

    game uploaded

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationManager : MonoBehaviour {
+
+    // Use this for initialization
+    public Animator PlayerAnimation;
+    public Animator PlayerAnimation_2;
+    public int myScore;
+    //public int Score_2;
+    public Text myText;
+    //public Text enemyText;
+
+    public Text text_2;
+
+    void Start () {
+        SetCountText ();
+        myScore = 0;
+      //  Score_2 = 0;
+        text_2.text = " ";
+		
+	}
+
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerAnimation.SetTrigger("attacking");
+            myScore++;
+        }
+        else
+        {
+            PlayerAnimation_2.SetTrigger("attacking");
+        //    Score_2++;
+        }
+        SetCountText();
+
+    }
+    void SetCountText()
+    {
+        myText.text = "Score: " + myScore.ToString();
+       // enemyText.text = "Score: " + Score_2.ToString();
+
+        if (myScore >= 30)
+        {
+            text_2.text = "You Win!";
+        }
+      //  else if (Score_2>=10)
+        {
+        //    text_2.text = "You Lose!";
+        }
+    }
+}
+
