5ab09689158e010a6ead66721514e8bd325cc00b
fatymahmed <e2107613@ceng.metu.edu.tr>
2018-09-04 15:36:54 +0300

    player 2 score added

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -10,16 +10,18 @@ public class AnimationManager : MonoBehaviour {
     public Animator PlayerAnimation;
     public Animator PlayerAnimation_2;
     public int myScore;
-    //public int Score_2;
+    public int Score_2;
     public Text myText;
-    //public Text enemyText;
+    public Text enemyText;
+    private float time = 0.0f;
+    public float interpolationPeriod = 1.0f;
 
     public Text text_2;
 
     void Start () {
         SetCountText ();
         myScore = 0;
-      //  Score_2 = 0;
+        Score_2 = 0;
         text_2.text = " ";
 		
 	}
@@ -27,32 +29,38 @@ public class AnimationManager : MonoBehaviour {
 
 
     // Update is called once per frame
-    void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            PlayerAnimation.SetTrigger("attacking");
-            myScore++;
-        }
-        else
-        {
-            PlayerAnimation_2.SetTrigger("attacking");
-        //    Score_2++;
-        }
-        SetCountText();
+    void Update ()
+    {
+   
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                PlayerAnimation.SetTrigger("attacking");
+                myScore++;
+            }
+            else
+            {
+            time += Time.deltaTime;
 
+            if (time >= interpolationPeriod)
+            {
+                time = time - interpolationPeriod;
+                PlayerAnimation_2.SetTrigger("attacking");
+                Score_2++;
+            }
+            SetCountText();
+        }
     }
     void SetCountText()
     {
         myText.text = "Score: " + myScore.ToString();
-       // enemyText.text = "Score: " + Score_2.ToString();
-
+        enemyText.text = "Score: " + Score_2.ToString();
         if (myScore >= 30)
         {
             text_2.text = "You Win!";
         }
-      //  else if (Score_2>=10)
+        else if (Score_2>=10)
         {
-        //    text_2.text = "You Lose!";
+            text_2.text = "You Lose!";
         }
     }
 }
